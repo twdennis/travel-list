4efdd39a0fff9f18187fdd3d8fd8811bada001d3
Tom <131278560+twdennis@users.noreply.github.com>
2024-01-17 12:36:51 +0000

    input added to Item

--- src/App.js
+++ src/App.js
@@ -80,6 +80,7 @@ function PackingList({ items, onDeleteItem }) {
 function Item({ item, onDeleteItem }) {
   return (
     <li>
+      <input type="checkbox" />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
